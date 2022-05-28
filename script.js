cf62666afee597a9b0486726f242de710fb682b5
Bohdan Chornopolskyi <61753348+bohdanchornopolskyi@users.noreply.github.com>
2022-05-28 22:21:57 +0300

    Upload project

---
+++ script.js
@@ -0,0 +1,10 @@
+var names=["Gulmira","Joel","maksimilijan","Rajesh","john","issabela","jason","Adam","josipa",];
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i][0].toLowerCase()==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
